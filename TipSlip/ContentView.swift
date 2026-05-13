58eb0f1753ce795a13a52567ceeedf1a72845dd0
Tristan Barnett <tristanwbarnettbu@gmail.com>
2026-05-12 21:38:10 -0400

    Initial Commit

---
+++ TipSlip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipSlip
+//
+//  Created by Tristan Barnett on 5/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipSlip/TipSlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipSlipApp.swift
+//  TipSlip
+//
+//  Created by Tristan Barnett on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct TipSlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
